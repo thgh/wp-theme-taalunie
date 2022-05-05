557281cf322470c192031378d677ac3344fc4f8b
Thomas Ghysels <info@thomasg.be>
2022-05-05 14:13:56 +0200

    Increase maximum page size for goals

--- inc/onderwijstermen-taxonomies.php
+++ inc/onderwijstermen-taxonomies.php
@@ -5,6 +5,14 @@ CPT UI: Export region taxonomy:
 {"region":{"name":"region","label":"Regio's","singular_label":"Regio","description":"","public":"true","publicly_queryable":"true","hierarchical":"false","show_ui":"true","show_in_menu":"true","show_in_nav_menus":"true","query_var":"true","query_var_slug":"","rewrite":"true","rewrite_slug":"","rewrite_withfront":"1","rewrite_hierarchical":"0","show_admin_column":"false","show_in_rest":"true","show_in_quick_edit":"","rest_base":"","rest_controller_class":"","labels":{"menu_name":"Regio's","all_items":"Alle regio's","edit_item":"","view_item":"","update_item":"","add_new_item":"","new_item_name":"","parent_item":"","parent_item_colon":"","search_items":"","popular_items":"","separate_items_with_commas":"","add_or_remove_items":"","choose_from_most_used":"","not_found":"","no_terms":"","items_list_navigation":"","items_list":""},"meta_box_cb":"","object_types":["term"]}}
  */
 
+function maximum_api_filter($query_params) {
+    $query_params['per_page']["maximum"]=1000;
+    return $query_params;
+}
+add_filter('rest_category_collection_params', 'maximum_api_filter');
+add_filter('rest_goal_collection_params', 'maximum_api_filter');
+
+
 function cptui_register_custom_post_types()
 {
     register_post_type('taak', [
