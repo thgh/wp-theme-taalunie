f352c435ec58d1f42db22edd568dc8220a373750
Thomas Ghysels <info@thomasg.be>
2022-05-11 13:08:05 +0200

    Revert reverse sorting

--- js/home.js
+++ js/home.js
@@ -76,12 +76,12 @@ new Vue({
     },
     async loadCategories() {
       this.categoriesPromise = wpFetch('/wp-json/wp/v2/categories?per_page=1000')
-      this.categories = (await this.categoriesPromise).reverse()
+      this.categories = await this.categoriesPromise
       persist('categories', this.categories)
     },
     async loadDoelen() {
       this.allGoalsPromise = wpFetch('/wp-json/wp/v2/doel?per_page=1000')
-      this.allGoals = (await this.allGoalsPromise).reverse()
+      this.allGoals = await this.allGoalsPromise
       persist('allGoals', this.allGoals)
     },
     back() {
