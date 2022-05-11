6c56cf2634f0d1da2b05ae47a2c1438ea5b319b5
Thomas Ghysels <info@thomasg.be>
2022-05-11 13:12:49 +0200

    Show section title if there are no subsections

--- home.php
+++ home.php
@@ -103,7 +103,7 @@ get_header();
           </label>
 
           <div class="category-section" v-for="child in categoryChildren" :id="child.slug">
-            <!-- <h3 class="category-section-title">{{child.name}}</h3> -->
+            <h3 class="category-section-title" v-if="!child.children.length">{{child.name}}</h3>
             <label class="goal-card" :class="{checked:ids.includes(goal.id)}" v-for="goal of child.goals">
               <div class="goal-card__title">
                 <input type="checkbox" :checked="ids.includes(goal.id)" @change="toggle(goal)" />
@@ -163,7 +163,7 @@ get_header();
             <div class="goal-card__title">
               <div class="goal-handle"></div>
               <input type="text" placeholder="Ik kan..." class="flex-grow-1" v-model="title" id="addgoaltitle" />
-‹‹w              <button type="submit" class="goal-add" :disabled="!title">Voeg toe</button>
+                <button type="submit" class="goal-add" :disabled="!title">Voeg toe</button>
             </div>
           </form>
         </div>
