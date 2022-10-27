963ab4ccdb98f1aa61bba9cbba2f286f524aa713
Thomas Ghysels <info@thomasg.be>
2022-10-27 21:24:00 +0200

    wip

--- page.php
+++ page.php
@@ -15,24 +15,17 @@
 get_header();
 
 global $post;
-$withSiblings = false;
-if ($post->post_parent) {
-	$siblings = wp_list_pages(['child_of' => $post->post_parent]);
-	$withSiblings = count($siblings) > 1;
-}
+$withSiblings = !!$post->post_parent;
+
 ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main <?php if ($withSiblings) echo ' has-siblings'; ?>">
 
 		<?php
 		if ($withSiblings) {
-			echo '<div class="siblings"> <ul class="siblings-list">';
-			foreach ($siblings as $sibling) {
-				?>
-				<li class="sibling"><?php echo $sibling->post_title; ?></li>
-				<?php
-			}
-			echo '</ul> </div>';
+			echo '<div class="siblings">';
+			wp_list_pages(['child_of' => $post->post_parent]);
+			echo '</div>';
 		} 
 		
 		while ( have_posts() ) :
